76a0e86195a356a5ef10424c31bde1dcf56df612
ito_kyohei <ito_kyohei@cyberagent.co.jp>
2016-11-11 14:05:43 +0900

    Add UIScrollView extension

---
+++ InfiniteView/UIScrollViewExtension.swift
@@ -0,0 +1,24 @@
+//
+//  UIScrollViewExtension.swift
+//  InfiniteView
+//
+//  Created by Kyohei Ito on 2016/11/10.
+//  Copyright © 2016年 Kyohei Ito. All rights reserved.
+//
+
+import UIKit
+
+extension UIScrollView {
+    var scrolling: Bool {
+        return isTracking || isDragging || isDecelerating
+    }
+    
+    func stopScroll() {
+        setContentOffset(contentOffset, animated: false)
+    }
+    
+    var validityContentOffset: CGPoint {
+        return CGPoint(x: contentOffset.x - frame.minX, y: contentOffset.y - frame.minY)
+    }
+    
+}
